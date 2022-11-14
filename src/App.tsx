c0dc57817c85700b84fcfcb76a47e613e07cb2fc
PietroMil <pietro.milanese@edu.itspiemonte.it>
2022-11-14 14:13:21 +0100

    creato build

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 function App() {
   const [email, setEmail] = useState("");
