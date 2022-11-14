e1b39646a743ab02798f1682537a8e76525325b5
PietroMil <pietro.milanese@edu.itspiemonte.it>
2022-11-14 22:35:48 +0100

    modifiche header

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,13 @@
 import { getAuth } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { app, auth } from "./firebase-config";
 import { signOut } from "firebase/auth";
 import Search from "./Search";
-
+import "./Header.css";
 export default function Home() {
-
-  console.log(auth.currentUser?.uid)
-
+  console.log(auth.currentUser?.uid);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   const handleLogout = () => {
     const auth = getAuth(app);
@@ -43,67 +42,125 @@ export default function Home() {
   let name = sessionStorage.getItem("email");
 
   return (
-  <>
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          className="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
-        <span className="font-semibold text-xl tracking-tight">Films</span>
-      </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+    <>
+      <div className="font-bold bg-purple-500 flex items-center justify-between border-b border-gray-400 p-4">
+        <div className="flex text-white">
+          {" "}
           <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
+            className="fill-white h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"></path>
           </svg>
-        </button>
-      </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Docs
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Examples
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-          >
-            Blog
-          </a>
+          <span className="font-semibold text-xl tracking-tight">Films</span>
         </div>
-        <div>
-          <span className="font-semibold text-xl tracking-tight">{name}</span>
 
-          <a
-            onClick={handleLogout}
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          >
-            LogOut
-          </a>
-        </div>
+        <nav>
+          <section className="MOBILE-MENU flex lg:hidden">
+            <div
+              className="cursor-pointer HAMBURGER-ICON space-y-2"
+              onClick={() => setIsNavOpen((prev) => !prev)}
+            >
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            </div>
+
+            <div
+              className={
+                isNavOpen ? "showMenuNav bg-purple-500" : "hideMenuNav"
+              }
+            >
+              <div
+                className="cursor-pointer absolute top-0 right-0 px-8 py-8"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <svg
+                  className="h-8 w-8 text-gray-600"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+
+              <ul className="text-white flex flex-col items-center justify-center min-h-[250px]">
+                <li className="mt-6">
+                  <div className="flex text-white">
+                    {" "}
+                    <svg
+                      className="fill-white h-8 w-8 mr-2"
+                      width="54"
+                      height="54"
+                      viewBox="0 0 54 54"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"></path>
+                    </svg>
+                    <span className="font-semibold text-xl tracking-tight">
+                      Films
+                    </span>
+                  </div>
+                </li>
+                <li className=" my-2 uppercase">
+                  <a href="/about">About</a>
+                </li>
+                <li className=" my-2 uppercase">
+                  <a href="/portfolio">Portfolio</a>
+                </li>
+                <li className=" my-2 uppercase">
+                  <a href="/contact">Contact</a>
+                </li>
+                <li>
+                  <span className="font-semibold text-xl tracking-tight">
+                    {name}
+                  </span>
+                </li>
+                <li>
+                  <a
+                    onClick={handleLogout}
+                    className="cursor-pointer inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+                  >
+                    LogOut
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <ul className="text-white DESKTOP-MENU hidden space-x-8 lg:flex">
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <span className="font-semibold">{name}</span>
+            </li>
+            <li>
+              <a
+                onClick={handleLogout}
+                className=" cursor-pointer inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              >
+                LogOut
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </nav>
-    <Search></Search>
+      <Search></Search>
     </>
-    
   );
 }
