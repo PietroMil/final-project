2c05491ad3fca0a5d2ea89d627d92efe1d103be8
PietroMil <pietro.milanese@edu.itspiemonte.it>
2022-11-30 10:13:14 +0100

    instllazione redux

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { initializeApp } from "firebase/app";
 import { config, app } from "./config/config";
-import AuthRoute from "./components/auth/AuthRoute";
 import DetailPage from "./pages/DetailPage";
 import Favorites from "./pages/Favorites";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import ProtectedRoute from "./components/auth/ProtectedPage";
 import { selecTheme } from "./theme/theme.slice";
-import { useAppSelector, useAppDispatch } from "./store/hooks";
-
+import { useAppSelector } from "./store/hooks";
 
 initializeApp(config.firebaseConfig);
 
@@ -24,7 +22,6 @@ onAuthStateChanged(auth, (user) => {
     const { uid, displayName } = user;
     const currentUser = { uid, displayName };
     localStorage.setItem("user", JSON.stringify(currentUser));
-    
   } else {
     localStorage.setItem("user", "");
   }
@@ -37,41 +34,36 @@ const App: React.FunctionComponent<InputAppProps> = () => {
 
   useEffect(() => {
     if (theme === "dark") {
-      
-      document.documentElement.classList.add('bg-black');
-      document.documentElement.classList.add('dark');
+      document.documentElement.classList.add("bg-black");
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('bg-black')
-      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove("bg-black");
+      document.documentElement.classList.remove("dark");
     }
   }, [theme]);
 
   return (
-    
-      <BrowserRouter>
-    
-        <Routes>
-          <Route path="/" element={<HomeUser />} />
-          <Route
-            path="/:showId"
-            element={
-              <ProtectedRoute
-                user={JSON.parse(
-                  localStorage.getItem("user") || '{"uid": false}'
-                )}
-              >
-                <DetailPage />
-              </ProtectedRoute>
-            }
-          />
-          
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-       
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeUser />} />
+        <Route
+          path="/:showId"
+          element={
+            <ProtectedRoute
+              user={JSON.parse(
+                localStorage.getItem("user") || '{"uid": false}'
+              )}
+            >
+              <DetailPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
